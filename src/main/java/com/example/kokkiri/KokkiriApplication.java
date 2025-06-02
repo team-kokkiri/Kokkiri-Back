f97412293f8f0635880c2765ca269033946c0ae4
keemzleun <92363679+keemzleun@users.noreply.github.com>
2025-06-02 17:52:22 +0900

    initial commit

---
+++ src/main/java/com/example/kokkiri/KokkiriApplication.java
@@ -0,0 +1,13 @@
+package com.example.kokkiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokkiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KokkiriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kokkiri/KokkiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kokkiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokkiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
