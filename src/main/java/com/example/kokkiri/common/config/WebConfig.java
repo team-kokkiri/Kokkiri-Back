59e6781d8b54209f4e5d30fdb2f9cd3efa55aa24
myjuniverse <rkskrh10@naver.com>
2025-07-03 15:56:42 +0900

    WIP

--- src/main/java/com/example/kokkiri/common/config/WebConfig.java
+++ src/main/java/com/example/kokkiri/common/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.example.kokkiri.common.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://192.160.230.207:8080") // Vue 개발 서버 주소
+                .allowedOrigins("http://localhost:8080", "http://192.168.230.8:8080") // Vue 개발 서버 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
@@ -22,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("classpath:/static/images/");
-        
+
         // 프로필 이미지 업로드 경로 추가
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
